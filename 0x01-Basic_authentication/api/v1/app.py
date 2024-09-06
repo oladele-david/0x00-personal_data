57d3d5580c5af9f576182ea9eb2029e0e62d0b7a
oladele.david <tdboy59@gmail.com>
2024-09-06 12:36:42 +0100

    Add 401 handler task 1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,6 +22,11 @@ def not_found(error) -> Tuple[Any, int]:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized(error) -> Tuple[Any, int]:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
